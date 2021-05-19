740940a4629ce6a57e12dd67788be089def03da5
bguiz <bguiz@users.noreply.github.com>
2021-05-19 18:27:30 +0800

    feat: impl controller than gets filters and options specific to rsk token bridge

---
+++ rsk-token-bridge-controller.js
@@ -0,0 +1,33 @@
+const filterByParams = require('./util/filter-by-params.js');
+const rskTokenBridgeFilters = require('./data/rsk-token-bridge-filters.json');
+const rskTokenBridgeOptions = require('./data/rsk-token-bridge-options.json');
+
+function getFilters(fromNetwork, walletName, txAge) {
+  const params = {
+    fromNetwork,
+    walletName,
+    txAge,
+  };
+  return filterByParams(rskTokenBridgeFilters, params);
+}
+
+function getOptions(fromNetwork, txHash, walletName, txAge) {
+  const filters = getFilters(fromNetwork, walletName, txAge);
+  const optionIds = new Set();
+  filters.forEach((filter) => {
+    filter.options.forEach((filterOption) => {
+      optionIds.add(filterOption);
+    });
+  });
+  const options = [...optionIds.values()].map((optionId) => {
+    return rskTokenBridgeOptions[optionId];
+  }).filter((option) => {
+    return !!option;
+  });
+  return options;
+}
+
+module.exports = {
+  getFilters,
+  getOptions,
+};
