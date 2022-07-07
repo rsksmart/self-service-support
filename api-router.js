e52439b11e011e6508f7dfe5b9c62a4f6d3e93b0
bguiz <github@bguiz.com>
2022-07-07 23:05:16 +0800

    proj: bump endpoint version to test CICD

--- api-router.js
+++ api-router.js
@@ -103,12 +103,12 @@ router.get('/rsk-address-report/protocol-usage', async (req, res) => {
     const { address, months } = req.query;
     const addressReport = await getAddressReport(address, months);
     res.status(200).json({
-      endPointVersion: 3,
+      endPointVersion: 4,
       ...addressReport
     });
   } catch (error) {
     res.status(400).json({
-      endPointVersion: 3,
+      endPointVersion: 4,
       error: error.message,
     });
   }
