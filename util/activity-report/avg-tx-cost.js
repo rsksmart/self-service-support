afec1f3d9a051f0b038767989355b857cc277f28
Aleksandr Shenshin <shenshin@me.com>
2022-11-26 23:37:52 +0100

    make cache for the avg tx cost api

--- util/activity-report/avg-tx-cost.js
+++ util/activity-report/avg-tx-cost.js
@@ -1,6 +1,20 @@
 const format = require('pg-format');
+const NodeCache = require('node-cache');
 const db = require('../../dbPool.js');
 const { getChainTableName } = require('./util.js');
+
+// cache time to live
+const shortStorageTtl = 600; // seconds
+const databaseDelayLimit = 3; // seconds
+
+const cache = new NodeCache();
+/* 
+  I use 2 types of cache storages:
+  - short term storage (ttl is specified at the file top)
+  - long term storage (unlimited time)
+ */
+const getShortStorageId = (chain) => `avg-tx-cost-short-storage-${chain}`;
+const getLongStorageId = (chain) => `avg-tx-cost-long-storage-${chain}`;
 
 async function dbQueryAvgTxCost(blocks, chainTableName) {
   /* 
@@ -32,26 +46,67 @@ async function dbQueryAvgTxCost(blocks, chainTableName) {
     WHERE t.fees_paid != 0  
   `;
   const query = format(queryStr, chainTableName, blocks, chainTableName);
-  return (await db.query(query)).rows[0];
+  const [avgTxCost] = (await db.query(query)).rows;
+  if (!avgTxCost?.avg_tx_cost_usd) throw new Error('DB records not found');
+  return {
+    time: new Date(),
+    ...avgTxCost,
+  };
+}
+
+function getFromDbOrLongStorage(blocks, chainTableName) {
+  return new Promise((resolve) => {
+    const getCached = () =>
+      resolve(cache.get(getLongStorageId(chainTableName)));
+    // try to get db response before the deadline
+    (async () => {
+      try {
+        const avgTxCost = await dbQueryAvgTxCost(blocks, chainTableName);
+        // each time we are able to successfully get the query result
+        // store these values + the timestamp in memory
+        cache.set(getLongStorageId(chainTableName), avgTxCost);
+        resolve(avgTxCost);
+      } catch (error) {
+        // each time we are unsuccessful in making the query (e.g. DB went down)
+        // retrieve the values plus timestamp from the long storage
+        getCached();
+      }
+    })();
+    // if waiting longer than deadline, read from the long storage
+    setTimeout(getCached, databaseDelayLimit * 1000);
+  });
+}
+
+function verifyParams(blocks) {
+  const blocksRange = {
+    lower: 1,
+    upper: 1000,
+  };
+  if (!(blocks >= blocksRange.lower && blocks <= blocksRange.upper))
+    throw new Error(
+      `Number of blocks should be within range ${blocksRange.lower} to ${blocksRange.upper}, specified value was: ${blocks}`,
+    );
 }
 
 async function queryAvgTxCost({ blocks = 100, chain = 'rsk_mainnet' }) {
-  if (!(blocks > 0 && blocks <= 1000))
-    throw new Error(`Illegal number of blocks: ${blocks}`);
+  verifyParams(blocks);
   const chainTableName = getChainTableName(chain);
-  const avgCosts = await dbQueryAvgTxCost(blocks, chainTableName);
-  // setting default values in case Covalent DB shuts down
-  const defaultTxCost = {
-    usd: 0.08150987210454078,
-    rbtc: 0.000004918220924135874,
-  };
+  // Try to read from short term storage.
+  // Less than 10 minutes after last DB query
+  // --> return cached value, no query performed
+  let avgTxCost = cache.get(getShortStorageId(chainTableName));
+  // When the short term storage ttl is expired:
+  // 10 minutes or longer --> perform the query, and return new value
+  if (!avgTxCost) {
+    // read from either DB or long storage (whichever available)
+    avgTxCost = await getFromDbOrLongStorage(blocks, chainTableName);
+    // write received data to short storage
+    cache.set(getShortStorageId(chainTableName), avgTxCost, shortStorageTtl);
+  }
   return {
     blocks,
     chain,
-    avg_tx_cost: {
-      usd: avgCosts?.avg_tx_cost_usd || defaultTxCost.usd,
-      rbtc: avgCosts?.avg_tx_cost_rbtc || defaultTxCost.rbtc,
-    },
+    ...avgTxCost,
   };
 }
 
