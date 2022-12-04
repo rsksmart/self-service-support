2edb128d9dd6cbfc328f46a28d2860398a2a3ef4
Aleksandr Shenshin <shenshin@me.com>
2022-12-04 16:20:18 +0100

    test of subtracting seconds from a Date object

--- tests/api-router.e2e.test.js
+++ tests/api-router.e2e.test.js
@@ -1,12 +1,12 @@
 const test = require('tape');
 const request = require('supertest');
 
-const server = require('./server.js');
+const server = require('../server.js');
 
 test('GET /api/v1/bogus-product-name/options', function (t) {
   request(server)
     .get('/api/v1/bogus-product-name/options')
-    .set({ 'Accept': 'application/json' })
+    .set({ Accept: 'application/json' })
     .expect(400)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
@@ -16,16 +16,17 @@ test('GET /api/v1/bogus-product-name/options', function (t) {
         error: 'unsupported product',
         value: ['bogus-product-name'],
       };
-      t.deepEqual(bodyActual, bodyExpected,
-        'Error message expected');
+      t.deepEqual(bodyActual, bodyExpected, 'Error message expected');
       t.end();
     });
 });
 
 test('GET /api/v1/rsk-token-bridge/options with invalid query params', function (t) {
   request(server)
-    .get('/api/v1/rsk-token-bridge/options?fromNetwork=another+chain&txHash=12345&walletName=beep+boop')
-    .set({ 'Accept': 'application/json' })
+    .get(
+      '/api/v1/rsk-token-bridge/options?fromNetwork=another+chain&txHash=12345&walletName=beep+boop',
+    )
+    .set({ Accept: 'application/json' })
     .expect(400)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
@@ -39,16 +40,17 @@ test('GET /api/v1/rsk-token-bridge/options with invalid query params', function 
           'invalid walletName: beep boop',
         ],
       };
-      t.deepEqual(bodyActual, bodyExpected,
-        'Response values expected');
+      t.deepEqual(bodyActual, bodyExpected, 'Response values expected');
       t.end();
     });
 });
 
 test.skip('GET /api/v1/rsk-token-bridge/options with valid query params (rsk-testnet)', function (t) {
   request(server)
-    .get('/api/v1/rsk-token-bridge/options?fromNetwork=rsk-testnet&txHash=0xf1ebb8076ad289fbaef4406bb0488be0c5605a58cfa2a6d11540b1f9b0d7ef98&walletName=metamask')
-    .set({ 'Accept': 'application/json' })
+    .get(
+      '/api/v1/rsk-token-bridge/options?fromNetwork=rsk-testnet&txHash=0xf1ebb8076ad289fbaef4406bb0488be0c5605a58cfa2a6d11540b1f9b0d7ef98&walletName=metamask',
+    )
+    .set({ Accept: 'application/json' })
     .expect(200)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
@@ -62,21 +64,24 @@ test.skip('GET /api/v1/rsk-token-bridge/options with valid query params (rsk-tes
         message: 'ok',
         properties: {
           fromNetwork: 'rsk-testnet',
-          txHash: '0xf1ebb8076ad289fbaef4406bb0488be0c5605a58cfa2a6d11540b1f9b0d7ef98',
+          txHash:
+            '0xf1ebb8076ad289fbaef4406bb0488be0c5605a58cfa2a6d11540b1f9b0d7ef98',
           walletName: 'metamask',
         },
       };
       t.deepEqual(bodyActual, bodyExpected, 'response match');
       t.ok(typeof txAgeActual === 'number', 'txAge present');
-      t.equal(optionsActual.length, 6, 'contains expected number of options')
+      t.equal(optionsActual.length, 6, 'contains expected number of options');
       t.end();
     });
 });
 
 test('GET /api/v1/rsk-token-bridge/options with valid query params (rsk-mainnet)', function (t) {
   request(server)
-    .get('/api/v1/rsk-token-bridge/options?fromNetwork=rsk-mainnet&txHash=0x573c70270258ce99acfc2baaa306fcdc88b1e50c3144d26a8ab74a2f21ea442a&walletName=nifty')
-    .set({ 'Accept': 'application/json' })
+    .get(
+      '/api/v1/rsk-token-bridge/options?fromNetwork=rsk-mainnet&txHash=0x573c70270258ce99acfc2baaa306fcdc88b1e50c3144d26a8ab74a2f21ea442a&walletName=nifty',
+    )
+    .set({ Accept: 'application/json' })
     .expect(200)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
@@ -90,22 +95,25 @@ test('GET /api/v1/rsk-token-bridge/options with valid query params (rsk-mainnet)
         message: 'ok',
         properties: {
           fromNetwork: 'rsk-mainnet',
-          txHash: '0x573c70270258ce99acfc2baaa306fcdc88b1e50c3144d26a8ab74a2f21ea442a',
+          txHash:
+            '0x573c70270258ce99acfc2baaa306fcdc88b1e50c3144d26a8ab74a2f21ea442a',
           walletName: 'nifty',
           txFrom: '0x0000000000000000000000000000000000000000',
         },
       };
       t.deepEqual(bodyActual, bodyExpected, 'response match');
       t.ok(typeof txAgeActual === 'number', 'txAge present');
-      t.equal(optionsActual.length, 8, 'contains expected number of options')
+      t.equal(optionsActual.length, 8, 'contains expected number of options');
       t.end();
     });
 });
 
 test('GET /api/v1/rsk-token-bridge/options with valid query params (ethereum-mainnet)', function (t) {
   request(server)
-    .get('/api/v1/rsk-token-bridge/options?fromNetwork=ethereum-mainnet&txHash=0x3985fe2ad509a4588501494a715957506f401364112bd55090529686aa538962&walletName=metamask')
-    .set({ 'Accept': 'application/json' })
+    .get(
+      '/api/v1/rsk-token-bridge/options?fromNetwork=ethereum-mainnet&txHash=0x3985fe2ad509a4588501494a715957506f401364112bd55090529686aa538962&walletName=metamask',
+    )
+    .set({ Accept: 'application/json' })
     .expect(200)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
@@ -119,14 +127,15 @@ test('GET /api/v1/rsk-token-bridge/options with valid query params (ethereum-mai
         message: 'ok',
         properties: {
           fromNetwork: 'ethereum-mainnet',
-          txHash: '0x3985fe2ad509a4588501494a715957506f401364112bd55090529686aa538962',
+          txHash:
+            '0x3985fe2ad509a4588501494a715957506f401364112bd55090529686aa538962',
           txFrom: '0x22cb463a75b3cb26fccd82684449117eda89fba0',
           walletName: 'metamask',
         },
       };
       t.deepEqual(bodyActual, bodyExpected, 'response match');
       t.ok(typeof txAgeActual === 'number', 'txAge present');
-      t.equal(optionsActual.length, 8, 'contains expected number of options')
+      t.equal(optionsActual.length, 8, 'contains expected number of options');
       t.end();
     });
 });
