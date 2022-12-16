289c388b9b666735ffcbaa1da3a2c01ff8409148
Aleksandr Shenshin <shenshin@me.com>
2022-12-16 16:14:30 +0100

    refactor qrcode react app

--- static-router.js
+++ static-router.js
@@ -5,15 +5,20 @@ const router = express.Router();
 
 const staticDir = join(__dirname, 'static');
 
+const maxAge = '1d';
+
 // qr-code generator
 const qrCodeDir = join(staticDir, 'qrcode');
-router.use(express.static(qrCodeDir));
+router.use(express.static(qrCodeDir, { maxAge }));
 router.get('/qrcode', (req, res) => {
-  res.sendFile(join(qrCodeDir, 'index.html'));
+  res.sendFile('index.html', {
+    root: qrCodeDir,
+    maxAge,
+  });
 });
 
 // rsk token bridge
 const tokenBridgeDir = join(staticDir, 'rsk-token-bridge');
-router.use('/rsk-token-bridge', express.static(tokenBridgeDir));
+router.use('/rsk-token-bridge', express.static(tokenBridgeDir, { maxAge }));
 
 module.exports = router;
